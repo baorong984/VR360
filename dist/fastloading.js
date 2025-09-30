344702ba4e60a85a2d0346b73c1063d10014fe22
Maicro-bao <baorong@airia.cn>
2025-09-30 15:38:53 +0800

    调整初始化部分代码，增加全景图片的切换逻辑。过滤重合的热点

--- dist/fastloading.js
+++ dist/fastloading.js
@@ -1,90 +1,113 @@
-var tpanoAutoLoad = [];
+const photo_list = [
+  {
+    url: "http://172.16.50.217:10081/image_api/DJI_20250923161259_0005_V.jpg",
+    name: "main",
+    img_id: 1,
+    geoReference: {
+      longitude: 118.930907,
+      latitude: 32.028434,
+      altitude: 10,
+    },
+  },
+  {
+    url: "http://172.16.50.217:10081/image_api/DJI_20250923161046_0003_V.jpg",
+    name: "0003",
+    img_id: 3,
+    geoReference: {
+      longitude: 118.931944,
+      latitude: 32.028096,
+      altitude: 10,
+    },
+  },
+];
 
-for (let i = 0; i < document.getElementsByTagName("tpano").length; i++) {
-  const panoElement = document.getElementsByTagName("tpano")[i];
+const pano = new TPano({
+  el: "tp",
+  photo: photo_list,
+  hotspot: [
+    {
+      source: "main",
+      geoReference: {
+        longitude: 118.931944,
+        latitude: 32.028096,
+        altitude: 10,
+      },
+      imgUrl: "http://172.16.50.217:10081/image_api/icon1.png",
+      jumpTo: true,
+      details: {
+        title: "10kV1057精汽线032-015号杆养殖小区1分支线#002",
+        img_id: 3,
+        content: "002",
+      },
+    },
+    {
+      source: "main",
+      geoReference: {
+        longitude: 118.930907,
+        latitude: 32.028434,
+        altitude: 10,
+      },
+      imgUrl: "http://172.16.50.217:10081/image_api/icon1.png",
+      jumpTo: true,
+      details: {
+        title: "10kV1057精汽线032-015号杆养殖小区1分支线#004",
+        img_id: 1,
+        content: "004",
+      },
+    },
+    {
+      source: "main",
+      geoReference: {
+        longitude: 118.928506,
+        latitude: 32.031822,
+        altitude: 10,
+      },
+      imgUrl: "http://172.16.50.217:10081/image_api/simple.png",
+      jumpTo: "A2",
+    },
+    {
+      source: "main",
+      geoReference: {
+        longitude: 118.929427,
+        latitude: 32.025522,
+        altitude: 10,
+      },
+      imgUrl: "http://172.16.50.217:10081/image_api/source.png",
+      jumpTo: "泉水新村",
+    },
+    {
+      source: "main",
+      geoReference: {
+        longitude: 118.922261,
+        latitude: 32.027616,
+        altitude: 10,
+      },
+      imgUrl: "http://172.16.50.217:10081/image_api/simple.png",
+      jumpTo: "中专",
+    },
+    {
+      source: "main",
+      geoReference: {
+        longitude: 118.936728,
+        latitude: 32.029724,
+        altitude: 10,
+      },
+      imgUrl: "http://172.16.50.217:10081/image_api/source.png",
+      jumpTo: "泉水公寓",
+    },
+  ],
+  rotateAnimateController: false,
+  MouseController: false,
+  debug: true,
+  onHotspotClick: function (hotspot) {
+    console.warn("点击了热点：", pano, hotspot);
+    const img_id = hotspot.img_id;
+    let index = photo_list.findIndex((item) => item.img_id == img_id);
 
-  // 获取地理参考信息
-  const geoRef = {
-    longitude: 118.930907,
-    latitude: 32.028434,
-    altitude: 10,
-  };
-
-  const pano = new TPano({
-    el: panoElement.id,
-    geoReference: geoRef, // 设置地理参考原点
-    photo: [
-      {
-        url: panoElement.attributes.src.value,
-        name: "main",
-        geoReference: geoRef,
-      },
-    ],
-    hotspot: [
-      {
-        source: "main",
-        geoReference: {
-          longitude: 118.931902,
-          latitude: 32.028099,
-          altitude: 10,
-        },
-        imgUrl: "http://172.16.50.217:10081/image_api/icon1.png",
-        jumpTo: true,
-        details: {
-          title: "10kV1057精汽线032-015号杆养殖小区1分支线#002",
-          content: "A2",
-        },
-      },
-      {
-        source: "main",
-        geoReference: {
-          longitude: 118.928506,
-          latitude: 32.031822,
-          altitude: 10,
-        },
-        imgUrl: "http://172.16.50.217:10081/image_api/simple.png",
-        jumpTo: "A2",
-      },
-      {
-        source: "main",
-        geoReference: {
-          longitude: 118.929427,
-          latitude: 32.025522,
-          altitude: 10,
-        },
-        imgUrl: "http://172.16.50.217:10081/image_api/source.png",
-        jumpTo: "泉水新村",
-      },
-      {
-        source: "main",
-        geoReference: {
-          longitude: 118.922261,
-          latitude: 32.027616,
-          altitude: 10,
-        },
-        imgUrl: "http://172.16.50.217:10081/image_api/simple.png",
-        jumpTo: "中专",
-      },
-      {
-        source: "main",
-        geoReference: {
-          longitude: 118.936728,
-          latitude: 32.029724,
-          altitude: 10,
-        },
-        imgUrl: "http://172.16.50.217:10081/image_api/source.png",
-        jumpTo: "泉水公寓",
-      },
-    ],
-    rotateAnimateController: false,
-    MouseController: false,
-    debug: true,
-  });
-
-  tpanoAutoLoad[i] = pano;
-
-  setTimeout(() => {}, 500);
-}
+    // 点击热点后，切换到对应的全景照片
+    pano.re.switchPhoto(index);
+  },
+});
 
 var el = window.document.body;
 window.document.body.onmouseover = function (event) {
@@ -93,17 +116,12 @@ window.document.body.onmouseover = function (event) {
     document.documentElement.style.overflow = "hidden";
     document.body.style.overflow = "hidden";
 
-    console.log(tpanoAutoLoad);
-    for (let i = 0; i < tpanoAutoLoad.length; i++) {
-      tpanoAutoLoad[i].re.seitchMouseController(true);
-    }
+    pano.re.seitchMouseController(true);
   } else {
     document.documentElement.style.overflow = "auto";
     document.body.style.overflow = "auto";
 
-    for (let i = 0; i < tpanoAutoLoad.length; i++) {
-      tpanoAutoLoad[i].re.seitchMouseController(false);
-    }
+    pano.re.seitchMouseController(false);
   }
 };
 
@@ -114,16 +132,11 @@ window.document.body.addEventListener("touchstart", function (event) {
     document.documentElement.style.overflow = "hidden";
     document.body.style.overflow = "hidden";
 
-    console.log(tpanoAutoLoad);
-    for (let i = 0; i < tpanoAutoLoad.length; i++) {
-      tpanoAutoLoad[i].re.seitchMouseController(true);
-    }
+    pano.re.seitchMouseController(true);
   } else {
     document.documentElement.style.overflow = "auto";
     document.body.style.overflow = "auto";
 
-    for (let i = 0; i < tpanoAutoLoad.length; i++) {
-      tpanoAutoLoad[i].re.seitchMouseController(false);
-    }
+    pano.re.seitchMouseController(false);
   }
 });
